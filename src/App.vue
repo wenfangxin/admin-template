aee45bc04460d56fe99a4324451a58f60ec7ce0f
coke <18220828184@163.com>
2020-10-29 12:20:37 +0800

    修改字体

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
   #app{
     width: 100%;
     height: 100%;
+    -webkit-font-smoothing: antialiased;
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   }
    /*改变菜单展开颜色*/
